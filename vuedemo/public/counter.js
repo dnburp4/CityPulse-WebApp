979d516400a7f40465dbfa8ed0ae9f112723273a
Dario Nicolas Burbano <da681bur@htwg-konstanz.de>
2024-11-28 11:23:47 +0100

    Änderungen Startseite Struktur

---
+++ vuedemo/public/counter.js
@@ -0,0 +1,8 @@
+
+let counterZero= 0; 
+
+function counter() {
+counterZero++; 
+document.getElementById("counterDemo").innerHTML= "counter demo: " + counterZero; 
+}
+
---
+++ vuedemo/src/App.vue
@@ -0,0 +1,28 @@
+<script setup>
+import {ref} from 'vue'
+const count = ref(0)
+</script>
+
+
+
+
+<template>
+
+
+<p>
+
+Wussten Sie count ist: {{ count }}
+
+</p>
+
+  <button  @click= "count++" >Count is: {{ count }}</button>
+
+
+</template>
+
+
+<style scoped>
+
+</style>
+
+
